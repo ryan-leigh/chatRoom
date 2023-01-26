2f51e6e4302e83fbe8f5bbab7387024cd231d2e8
ryan-leigh <ryanleigh25@gmail.com>
2023-01-25 16:44:55 -0800

    Db setup

---
+++ server/sql/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS rooms CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+
+CREATE TABLE rooms (
+  id serial,
+  "name" text,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id serial,
+  "name" text,
+  email text,
+  updatedAt int,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE messages (
+  id serial,
+  user_id int,
+  room_id int,
+  body text,
+  time_created int,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_user_id
+    FOREIGN KEY(user_id)
+      REFERENCES users(id),
+  CONSTRAINT fk_room_id
+    FOREIGN KEY(room_id)
+      REFERENCES rooms(id)
+);
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const path = require('path');
@@ -5,15 +6,20 @@ const app = express();
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const server = new ApolloServer({ typeDefs, resolvers });
-
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+const port = process.env.PORT;
 
 const start = async () => {
   await server.start()
-    .then(() => server.applyMiddleware({ app }));
+    .then(() => {
+      app.use(express.static(path.join(__dirname, '../../client/dist')));
+    })
+    .then(() => {
+      server.applyMiddleware({ app });
+    })
+    .then(() => {
+      app.listen({ port }, () => {
+        console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
+      });
+    });
 }
 start();
-
-app.listen({ port: 3000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
-);
