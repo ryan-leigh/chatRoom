bb95fba0f62fe7899b609940bf9dda35bd711c84
ryan-leigh <ryanleigh25@gmail.com>
2023-02-01 17:45:04 -0800

    Add http and ws servers

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const path = require('path');
-const app = express();
 const MyDatabase = require('./datasources/db.js');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const express = require('express');
+const path = require('path');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { createServer } = require('http');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+
+// Create schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Set up data sources
 const knexConfig = {
   client: "postgres",
   connection: {
@@ -15,15 +26,37 @@ const knexConfig = {
   }
 }
 const db = new MyDatabase(knexConfig);
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({ db })
-});
-const port = process.env.PORT;
 
+// Create servers
+const port = process.env.PORT;
+const app = express();
+const httpServer = createServer(app);
+const apolloServer = new ApolloServer({
+  schema,
+  dataSources: () => ({ db }),
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ]
+});
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql'
+});
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Start servers
 const start = async () => {
   await apolloServer.start()
     .then(() => {
@@ -33,7 +66,7 @@ const start = async () => {
       apolloServer.applyMiddleware({ app });
     })
     .then(() => {
-      app.listen({ port }, () => {
+      httpServer.listen({ port }, () => {
         console.log(`🚀 Server ready at http://localhost:3000${apolloServer.graphqlPath}`);
       });
     });
